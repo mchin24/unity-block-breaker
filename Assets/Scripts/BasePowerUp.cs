4e0ceffd4bbfd63f2393a69dd9997ffe489767f4
Mike Chin <mchin24@gmail.com>
2025-07-25 11:22:55 -0400

    Added BasePowerUp class - initial implementation

---
+++ Assets/Scripts/BasePowerUp.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D), typeof(AudioSource))]
+public class BasePowerUp : MonoBehaviour
+{
+    public float dropSpeed = 1;
+    public AudioClip sound;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+    }
+
+    IEnumerator OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Paddle")
+        {
+            OnPickup();
+            
+            GetComponent<Collider2D>().enabled = false;
+            GetComponent<Renderer>().enabled = false;
+            
+            GetComponent<AudioSource>().pitch = Time.timeScale;
+            GetComponent<AudioSource>().PlayOneShot(sound);
+            
+            yield return new WaitForSeconds(sound.length);
+        }
+    }
+
+    protected virtual void OnPickup()
+    {
+        
+    }
+}
