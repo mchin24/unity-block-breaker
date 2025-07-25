62d6d00483e50b52deec8212cb00b042f68fe9b7
Mike Chin <mchin24@gmail.com>
2025-07-25 11:23:07 -0400

    Added PowerUpDrop class

---
+++ Assets/Scripts/PowerUpDrop.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]    
+public class PowerUpDrop : MonoBehaviour
+{
+    public BasePowerUp PowerUpPrefab;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Instantiate(PowerUpPrefab, transform.position,  Quaternion.identity);
+    }
+}
