a7e1450cdace0b635d83eda3105322590720ce32
Mike Chin <mchin24@gmail.com>
2025-07-28 15:40:04 -0400

    Created a LevelManager class - When player wins the game, display an appropriate message and level completion time. After 5 seconds, return to the main menu.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject[] bricks;
+    public int count = 0;
+    private GameManager _gameManager;
+    public string finishTime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bricks = GameObject.FindGameObjectsWithTag("Brick");
+        count = bricks.Length;
+        Debug.Log("Brick count: " + count);
+
+        if (count == 0)
+        {
+            Debug.Log("All bricks are gone");
+            StartCoroutine(Pause());
+        }
+    }
+
+    IEnumerator Pause()
+    {
+        _gameManager = GameObject.FindObjectOfType<GameManager>();
+        _gameManager.SwitchState(GameState.Completed);
+        _gameManager.ChangeText("You Win!");
+        finishTime = _gameManager.formattedTime;
+        
+        Debug.Log("Total time: " + finishTime);
+        
+        yield return new WaitForSeconds(5);
+        
+        LoadScene(0);
+    }
+
+    public void LoadScene(int level)
+    {
+        LevelLoader levelLoader = new LevelLoader();
+        levelLoader.LoadScene(level);
+    }
+}
