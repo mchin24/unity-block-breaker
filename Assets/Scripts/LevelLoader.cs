943d07262f95dbab6b684eddfce4887157476312
Mike Chin <mchin24@gmail.com>
2025-07-30 16:50:13 -0400

    Added a quit button - Quit button closes the application on desktop, but is otherwise non-functional in web. Would be better to just not display it on web, but we'll get to that.

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -5,8 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
+    public GameObject quitButton;
     public void LoadScene(int level)
     {
         SceneManager.LoadScene(level);
-    } 
+    }
+    public void QuitGame()
+    {
+        if (Application.isEditor)
+        {
+            Debug.Log("Attempted to quit from the Editor.");
+        } else if (Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            quitButton = GameObject.FindGameObjectWithTag("Quit");
+            quitButton.SetActive(false);
+            Debug.Log("Attempted to quit from the WebGL Player.");
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
 }
